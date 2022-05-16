7ad73126dc771f1c99f04aa3b062a2018e1a2bdf
lucastine <lucastine@poli.ufrj.br>
2022-05-16 01:43:54 -0300

    first schematic-binarizing approach implemented

---
+++ src/photo_to_binary.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def binarize (image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred_image = cv2.GaussianBlur(gray_image, (5,5), 1)
+    threshold_image = cv2.Canny (blurred_image, 30, 70) 
+    kernel = np.ones ((2, 2))
+    closed_threshold = cv2.morphologyEx (threshold_image, cv2.MORPH_CLOSE, kernel, iterations=3)
+    return closed_threshold
+
+samples = None
+final = None
+for i in range (1,6): 
+    photo_path = f'images/sample{i}.jpg'
+    image = cv2.imread(photo_path)
+    binarized = cv2.resize(binarize (image), (1000, 1000))
+    resized = cv2.resize (image, (1000,1000))
+    samples = resized if samples is None else np.concatenate([samples, resized], axis=1)
+    final = binarized if final is None else np.c_[final, binarized]
+
+samples = cv2.cvtColor(samples, cv2.COLOR_BGR2RGB)
+final =  cv2.cvtColor(final, cv2.COLOR_GRAY2RGB)
+plt.imshow (np.concatenate([final, samples]))
+plt.axis('off')
+plt.show()
