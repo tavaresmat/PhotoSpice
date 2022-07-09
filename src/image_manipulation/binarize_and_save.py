6e82fddb92412ef0ea7ef104e2a0dbcd62822808
lucastine <lucastine@poli.ufrj.br>
2022-07-09 10:52:01 -0300

    added new data to numbers dataset

--- src/image_manipulation/binarize_and_save.py
+++ src/image_manipulation/binarize_and_save.py
@@ -4,15 +4,16 @@ import cv2
 
 from src.image_manipulation.utils import binarize
 
-if __name__ == '__main__':
-    color_images_dir = "dataset/cimages"
-    binary_images_dir = "dataset/bimages"
-    for file in os.listdir(color_images_dir):
-        filepath = os.path.join(color_images_dir, file)
+COLOR_SOURCE_DIR = "other_datasets/numbers/old_images"
+BINARY_DESTINATION_DIR = "test bin"
+
+if __name__ == '__main__':    
+    for file in os.listdir(COLOR_SOURCE_DIR):
+        filepath = os.path.join(COLOR_SOURCE_DIR, file)
         image = cv2.imread(filepath)
         binary_image = binarize(image)
         cv2.imwrite (os.path.join(
-                binary_images_dir,
+                BINARY_DESTINATION_DIR,
                 file
             ),
             binary_image
