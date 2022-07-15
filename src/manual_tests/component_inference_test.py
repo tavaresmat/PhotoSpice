e1837839039dbc18531b25a914b21305734d6070
Matheus Felinto <tavaresmat@poli.ufrj.br>
2022-07-15 16:58:50 -0300

    Add new models for character detection

--- src/manual_tests/component_inference_test.py
+++ src/manual_tests/component_inference_test.py
@@ -2,7 +2,7 @@ from src.yolo_inference.component_detector import ComponentDetector
 import cv2
 
 IMAGE = 'drawing.png'
-detector = ComponentDetector(weights='models/best.pt')
+detector = ComponentDetector(weights='models/characters_mAP.87.pt')
 image = cv2.imread(IMAGE)
 detection = detector.predict (image)
 print (detection)
--- src/manual_tests/label_plot_test.py
+++ src/manual_tests/label_plot_test.py
@@ -1,10 +1,11 @@
 from src.augmentation.bbox_manipulation import plot_bbox_from_path
 
-img = 'samplei3.var3'
+img = 'img1'
 imgext = 'png'
-_set = 'validation_aug'
-imagepath = f'teste charac/characters_bin/0-055.e3.png' # image that will be tested for augmentation
-bboxespath = f'teste charac/labels/0-055.e3.txt' # bboxes file in yolo format
+_set = 'train'
+dataset_name = "numbers2"
+imagepath = f'other_datasets/{dataset_name}/{_set}/images/{img}.{imgext}' # image that will be tested for augmentation
+bboxespath = f'other_datasets/{dataset_name}/{_set}/labels/{img}.txt' # bboxes file in yolo format
 
 plot_bbox_from_path (
     imagepath,
